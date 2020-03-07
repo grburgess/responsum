b7f3e30e6b0b8a40d3fb53ef201086800186ade8
J. Michael Burgess <jmichaelburgess@gmail.com>
2020-03-07 17:12:04 +0100

    switch to new version system

--- responsum/__init__.py
+++ responsum/__init__.py
@@ -1,3 +1,7 @@
 from responsum.response import OGIPResponse, InstrumentResponse
 
 __all__ = ['OGIPResponse', 'InstrumentResponse']
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
--- setup.py
+++ setup.py
@@ -1,130 +1,16 @@
-from setuptools.command.build_ext import build_ext as _build_ext
-from setuptools import find_packages, Command, setup
+from setuptools import setup, find_packages, Command
 import os
 import io
 import sys
 from shutil import rmtree
-import numpy
-#extensions_cython = [ext2]
+import versioneer
 
-# Package meta-data.
-NAME = 'responsum'
-DESCRIPTION = 'A generic dispersion response library'
-URL = 'https://github.com/mpe-grb/responsum'
-EMAIL = 'jmichaelburgess@gmail.com'
-AUTHOR = 'J. Michael Burgess'
-REQUIRES_PYTHON = '>=2.7.0'
-VERSION = None
+setup_requires = ["setuptools >= 30.3.0"]
 
-REQUIRED = [
-    'numpy',
-    'astropy',
-    'scipy',
-    'h5py',
-    'matplotlib'
-]
-
-# What packages are optional?
-EXTRAS = {
-    # 'fancy feature': ['django'],
-}
-
-here = os.path.abspath(os.path.dirname(__file__))
-
-try:
-    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-        long_description = '\n' + f.read()
-except FileNotFoundError:
-    long_description = DESCRIPTION
-
-# Load the package's __version__.py module as a dictionary.
-about = {}
-if not VERSION:
-    with open(os.path.join(here, NAME, '__version__.py')) as f:
-        exec(f.read(), about)
-else:
-    about['__version__'] = VERSION
-
-
-class UploadCommand(Command):
-    """Support setup.py upload."""
-
-    description = 'Build and publish the package.'
-    user_options = []
-
-    @staticmethod
-    def status(s):
-        """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        try:
-            self.status('Removing previous builds...')
-            rmtree(os.path.join(here, 'dist'))
-        except OSError:
-            pass
-
-        self.status('Building Source and Wheel (universal) distribution...')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(
-            sys.executable))
-
-        self.status('Uploading the package to PyPI via Twine...')
-        os.system('twine upload dist/*')
-
-        self.status('Pushing git tags...')
-        os.system('git tag v{0}'.format(about['__version__']))
-        os.system('git push --tags')
-
-        sys.exit()
-
-
-# Create list of data files
-def find_data_files(directory):
-
-    paths = []
-
-    for (path, directories, filenames) in os.walk(directory):
-
-        for filename in filenames:
-
-            paths.append(os.path.join('..', path, filename))
-
-    return paths
-
-
-extra_files = find_data_files('responsum/data')
-
-# setup(
-
-#     ext_modules=cythonize(extensions_cython), requires=['numpy']
-# )
 
 setup(
-    name=NAME,
-    version=about['__version__'],
-    description=DESCRIPTION,
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    author=AUTHOR,
-    author_email=EMAIL,
-    python_requires=REQUIRES_PYTHON,
-    url=URL,
-    packages=find_packages(exclude=('tests', )),
-    install_requires=REQUIRED,
-    extras_require=EXTRAS,
-    include_package_data=True,
-    package_data={
-        '': extra_files,
-    },
-    license='GPL3',
-
-    cmdclass={
-        'upload': UploadCommand,
-    },
+    setup_requires=setup_requires,
+    license="GPL3",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
 )
